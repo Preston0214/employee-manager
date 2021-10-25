aec979aa7a54ed669975051a663cf3c6ea57caf5
Preston <prestonconley5@gmail.com>
2021-10-24 20:38:46 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DELETE IF EXISTS company_db
+CREATE DATABASE company_db
+
+CREATE TABLE employees
+    id INT PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    role_id INT,
+    manager_id INT;
+
+CREATE TABLE roles
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT;
+
+CREATE TABLE department
+    id INT PRIMARY KEY,
+    name VARCHAR(20);
